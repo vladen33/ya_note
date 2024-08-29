49e0c31a3e7d25007cfaecb0ab0f86d4feda9890
Vlad Vasiliev <vlad@example.com>
2024-08-29 17:16:40 +0300

    Notes-Work  4%

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,116 @@
+import pytest
+
+from http import HTTPStatus
+
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+
+def test_home_availability_for_anonymous_user(client):
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete')
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, kwargs={'slug': note.slug})
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    # Предварительно оборачиваем имена фикстур
+    # в вызов функции pytest.lazy_fixture().
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+
+@pytest.mark.parametrize(
+    # Вторым параметром передаём note_object,
+    # в котором будет либо фикстура с объектом заметки, либо None.
+    'name, note_object',
+    (
+        ('notes:detail', pytest.lazy_fixture('note')),
+        ('notes:edit', pytest.lazy_fixture('note')),
+        ('notes:delete', pytest.lazy_fixture('note')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и note_object:
+def test_redirects(client, name, note_object):
+    login_url = reverse('users:login')
+    # Формируем URL в зависимости от того, передан ли объект заметки:
+    if note_object is not None:
+        url = reverse(name, args=(note_object.slug,))
+    else:
+        url = reverse(name)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    # Ожидаем, что со всех проверяемых страниц анонимный клиент
+    # будет перенаправлен на страницу логина:
+    assertRedirects(response, expected_url)
+
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и args:
+def test_redirects_var2(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
+
+
